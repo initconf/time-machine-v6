7400fbdffb667b33c89b627bb0648ec1750a9c23
John <john@john-OptiPlex-980.(none)>
2013-08-01 10:39:46 -0700

    An IPv6 supporting class is added and the Connection class is modified to use this class for ALL ip addresses, including IPv4

--- src/Connection.cc
+++ src/Connection.cc
@@ -1,20 +1,221 @@
 #include "types.h"
-
+#include <string>
+#include <vector>
 #include <pcap.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sstream>
 #include "re2/re2.h"
-
+//#include "IP6Address.hh"
 #include "Connection.hh"
 #include "packet_headers.h"
 #include "Fifo.hh"
 #include "Query.hh"
 #include "tm.h"
 
+
 static std::string pattern_ip ("(\\d+\\.\\d+\\.\\d+\\.\\d+)");
 static std::string pattern_ipport ("(\\d+\\.\\d+\\.\\d+\\.\\d+):(\\d+)");
 
+const H3<uint32_t, UHASH_KEY_SIZE>* h3fcn;
+
+
+
+
+
+
+
+const uint8_t IP6Address::v4_mapped_prefix[12] = { 0, 0, 0, 0,
+                                                   0, 0, 0, 0,
+                                                   0, 0, 0xff, 0xff };
+
+
+void IP6Address::Mask(int top_bits_to_keep)
+	{
+	if ( top_bits_to_keep < 0 || top_bits_to_keep > 128 )
+		{
+		//reporter->Error("Bad IP6Address::Mask value %d", top_bits_to_keep);
+		return;
+		}
+
+	uint32_t tmp[4];
+	memcpy(tmp, in6.s6_addr, sizeof(in6.s6_addr));
+
+	int word = 3;
+	int bits_to_chop = 128 - top_bits_to_keep;
+
+	while ( bits_to_chop >= 32 )
+		{
+		tmp[word] = 0;
+		--word;
+		bits_to_chop -= 32;
+		}
+
+	uint32_t w = ntohl(tmp[word]);
+	w >>= bits_to_chop;
+	w <<= bits_to_chop;
+	tmp[word] = htonl(w);
+
+	memcpy(in6.s6_addr, tmp, sizeof(in6.s6_addr));
+	}
+
+void IP6Address::ReverseMask(int top_bits_to_chop)
+	{
+	if ( top_bits_to_chop < 0 || top_bits_to_chop > 128 )
+		{
+		//reporter->Error("Bad IP6Address::ReverseMask value %d", top_bits_to_chop);
+		return;
+		}
+
+	uint32_t tmp[4];
+	memcpy(tmp, in6.s6_addr, sizeof(in6.s6_addr));
+
+	int word = 0;
+	int bits_to_chop = top_bits_to_chop;
+
+	while ( bits_to_chop >= 32 )
+		{
+		tmp[word] = 0;
+		++word;
+		bits_to_chop -= 32;
+		}
+
+	uint32_t w = ntohl(tmp[word]);
+	w <<= bits_to_chop;
+	w >>= bits_to_chop;
+	tmp[word] = htonl(w);
+
+	memcpy(in6.s6_addr, tmp, sizeof(in6.s6_addr));
+	}
+
+void IP6Address::init(const std::string& s)
+	{
+	if ( s.find(':') == std::string::npos ) // IPv4.
+		{
+		memcpy(in6.s6_addr, v4_mapped_prefix, sizeof(v4_mapped_prefix));
+
+		// Parse the address directly instead of using inet_pton since
+		// some platforms have more sensitive implementations than others
+		// that can't e.g. handle leading zeroes.
+		int a[4];
+		int n = sscanf(s.c_str(), "%d.%d.%d.%d", a+0, a+1, a+2, a+3);
+
+		if ( n != 4 || a[0] < 0 || a[1] < 0 || a[2] < 0 || a[3] < 0 ||
+		     a[0] > 255 || a[1] > 255 || a[2] > 255 || a[3] > 255 )
+			{
+			//reporter->Error("Bad IP address: %s", s.c_str());
+			memset(in6.s6_addr, 0, sizeof(in6.s6_addr));
+			return;
+			}
+
+		uint32_t addr = (a[0] << 24) | (a[1] << 16) | (a[2] << 8) | a[3];
+		addr = htonl(addr);
+		memcpy(&in6.s6_addr[12], &addr, sizeof(uint32_t));
+		}
+
+	else
+		{
+		if ( inet_pton(AF_INET6, s.c_str(), in6.s6_addr) <=0 )
+			{
+			//reporter->Error("Bad IP address: %s", s.c_str());
+			memset(in6.s6_addr, 0, sizeof(in6.s6_addr));
+			}
+		}
+	}
+
+std::string IP6Address::AsString() const
+	{
+	if ( GetFamily() == IPv4 )
+		{
+		char s[INET_ADDRSTRLEN];
+
+		if ( ! inet_ntop(AF_INET, &in6.s6_addr[12], s, INET_ADDRSTRLEN) )
+			return "<bad IPv4 address conversion";
+		else
+			return s;
+		}
+	else
+		{
+		char s[INET6_ADDRSTRLEN];
+
+		if ( ! inet_ntop(AF_INET6, in6.s6_addr, s, INET6_ADDRSTRLEN) )
+			return "<bad IPv6 address conversion";
+		else
+			return s;
+		}
+	}
+
+std::string IP6Address::AsHexString() const
+	{
+	char buf[33];
+
+	if ( GetFamily() == IPv4 )
+		{
+		uint32_t* p = (uint32_t*) &in6.s6_addr[12];
+		snprintf(buf, sizeof(buf), "%08x", (uint32_t) ntohl(*p));
+		}
+	else
+		{
+		uint32_t* p = (uint32_t*) in6.s6_addr;
+		snprintf(buf, sizeof(buf), "%08x%08x%08x%08x",
+				(uint32_t) ntohl(p[0]), (uint32_t) ntohl(p[1]),
+				(uint32_t) ntohl(p[2]), (uint32_t) ntohl(p[3]));
+		}
+
+	return buf;
+	}
+
+std::string IP6Address::PtrName() const
+	{
+	if ( GetFamily() == IPv4 )
+		{
+		char buf[256];
+		uint32_t* p = (uint32_t*) &in6.s6_addr[12];
+		uint32_t a = ntohl(*p);
+		uint32_t a3 = (a >> 24) & 0xff;
+		uint32_t a2 = (a >> 16) & 0xff;
+		uint32_t a1 = (a >> 8) & 0xff;
+		uint32_t a0 = a & 0xff;
+		snprintf(buf, sizeof(buf), "%u.%u.%u.%u.in-addr.arpa", a0, a1, a2, a3);
+		return buf;
+		}
+	else
+		{
+		static const char hex_digit[] = "0123456789abcdef";
+		std::string ptr_name("ip6.arpa");
+		uint32_t* p = (uint32_t*) in6.s6_addr;
+
+		for ( unsigned int i = 0; i < 4; ++i )
+			{
+			uint32_t a = ntohl(p[i]);
+			for ( unsigned int j = 1; j <=8; ++j )
+				{
+				ptr_name.insert(0, 1, '.');
+				ptr_name.insert(0, 1, hex_digit[(a >> (32-j*4)) & 0x0f]);
+				}
+			}
+
+		return ptr_name;
+		}
+	}
+
+
+
+/****************************************
+*****************************************
+****************************************/
+
+
+void init_H3()
+	{
+	tmlog(TM_LOG_NOTE, "init_H3", "******** h3fcn has been created!!!!!!!! *********");
+	h3fcn = new H3<uint32_t, UHASH_KEY_SIZE>();
+	}
+
+uint32_t hashword(
+const uint32_t *k,                   /* the key, an array of uint32_t values */
+size_t          length,               /* the length of the key, in uint32_ts */
+uint32_t        initval);         /* the previous hash, or an arbitrary value */
 inline uint32_t revert_uint32(uint32_t i) {
 	uint32_t r;
 	((uint8_t*)&r)[0]=((uint8_t*)&i)[3];
@@ -33,7 +234,7 @@ inline uint16_t revert_uint16(uint16_t i) {
 	return r;
 }
 
-inline bool addr_port_canon_lt(uint32_t s_ip, uint32_t d_ip,
+inline bool addr_port_canon_lt(IP6Address s_ip, IP6Address d_ip,
 							   uint16_t s_port, uint16_t d_port) {
 	if (s_ip == d_ip)
 		return (s_port < d_port);
@@ -41,8 +242,71 @@ inline bool addr_port_canon_lt(uint32_t s_ip, uint32_t d_ip,
 		return (s_ip < d_ip);
 }
 
+
+uint32_t IP6Address::hash() const {
+	in6_addr ip1;
+	memcpy(ip1.s6_addr, in6.s6_addr, sizeof(in6.s6_addr));
+	return (*h3fcn)(&ip1, sizeof(ip1));
+}
+
+
+uint32_t ConnectionID4::hash() const { 
+	//TODO: initval
+
+	struct {
+		in6_addr ip1;
+		in6_addr ip2;
+		uint16_t port1;
+		uint16_t port2;
+		proto_t proto;
+	} __attribute__((packed)) key ;
+
+	v.ip1.CopyIP6Address(&key.ip1);
+	v.ip2.CopyIP6Address(&key.ip2);
+	key.port1 = v.port1;
+	key.port2 = v.port2;
+	key.proto = v.proto;
+
+	return (*h3fcn)(&key, sizeof(key));
+}
+
+uint32_t ConnectionID3::hash() const { 
+	//TODO: initval
+
+	struct {
+		in6_addr ip1;
+		in6_addr ip2;
+		uint16_t port2;
+		proto_t proto;
+	} __attribute__((packed)) key;
+
+	v.ip1.CopyIP6Address(&key.ip1);
+	v.ip2.CopyIP6Address(&key.ip2);
+	key.port2 = v.port2;
+	key.proto = v.proto;
+
+	return (*h3fcn)(&key, sizeof(key));
+}
+
+uint32_t ConnectionID2::hash() const { 
+	//TODO: initval
+
+	struct {
+		in6_addr ip1;
+		in6_addr ip2;	
+	} __attribute__((packed)) key;
+
+	v.ip1.CopyIP6Address(&key.ip1);
+	v.ip2.CopyIP6Address(&key.ip2);
+
+	return (*h3fcn)(&key, sizeof(key));
+}
+
+
+
+
 void ConnectionID4::init(proto_t proto,
-						 uint32_t s_ip, uint32_t d_ip,
+						 IP6Address s_ip, IP6Address d_ip,
 						 uint16_t s_port, uint16_t d_port) {
 	v.proto=proto;
 	if (addr_port_canon_lt(s_ip,d_ip,s_port,d_port)) {
@@ -61,7 +325,7 @@ void ConnectionID4::init(proto_t proto,
 }
 
 void ConnectionID3::init(proto_t proto,
-						 uint32_t ip1, uint32_t ip2,
+						 IP6Address ip1, IP6Address ip2,
 						 uint16_t port2) {
 	v.proto=proto;
 	v.ip1=ip1;
@@ -69,7 +333,7 @@ void ConnectionID3::init(proto_t proto,
 	v.port2=port2;
 }
 
-void ConnectionID2::init( uint32_t s_ip, uint32_t d_ip) {
+void ConnectionID2::init(IP6Address s_ip, IP6Address d_ip) {
 	if (addr_port_canon_lt(s_ip,d_ip,0,0)) {
 		//    v.is_canonified=true;
 		v.ip1=d_ip;
@@ -81,66 +345,156 @@ void ConnectionID2::init( uint32_t s_ip, uint32_t d_ip) {
 	}
 }
 
+//******NEEDS TO TEST FOR IPV6*/	
 ConnectionID4::ConnectionID4(const u_char* packet) {
-	switch (IP(packet)->ip_p) {
-	case IPPROTO_UDP:
-		init(IP(packet)->ip_p,
-			 IP(packet)->ip_src.s_addr, IP(packet)->ip_dst.s_addr,
-			 UDP(packet)->uh_sport, UDP(packet)->uh_dport);
-		break;
-	case IPPROTO_TCP:
-		init(IP(packet)->ip_p,
-			 IP(packet)->ip_src.s_addr, IP(packet)->ip_dst.s_addr,
-			 TCP(packet)->th_sport, TCP(packet)->th_dport);
-		break;
+
+	switch (IPV(packet)) {
+	case IPv4:
+		switch (IP(packet)->ip_p) {
+		case IPPROTO_UDP:
+			init(IP(packet)->ip_p,
+				 IP6Address(IP(packet)->ip_src.s_addr), 
+				 IP6Address(IP(packet)->ip_dst.s_addr),
+				 UDP(packet)->uh_sport, UDP(packet)->uh_dport);
+			break;
+		case IPPROTO_TCP:
+			init(IP(packet)->ip_p,
+				 IP6Address(IP(packet)->ip_src.s_addr), 
+				 IP6Address(IP(packet)->ip_dst.s_addr),
+				 TCP(packet)->th_sport, TCP(packet)->th_dport);
+			break;
+		default:
+			init(IP(packet)->ip_p,
+				 IP6Address(IP(packet)->ip_src.s_addr), 
+				 IP6Address(IP(packet)->ip_dst.s_addr), 0, 0);
+			break;
+		} break;
+	case IPv6:
+		switch (IP6_NXT_HDR(packet)) {
+		case IPPROTO_UDP:
+			init(IP6_NXT_HDR(packet),
+				 IP6Address(IP6(packet)->ip6_src), 
+				 IP6Address(IP6(packet)->ip6_dst),
+				 UDP(packet)->uh_sport, UDP(packet)->uh_dport);
+			break;
+		case IPPROTO_TCP:
+			init(IP6_NXT_HDR(packet),
+				 IP6Address(IP6(packet)->ip6_src), 
+				 IP6Address(IP6(packet)->ip6_dst),
+				 TCP(packet)->th_sport, TCP(packet)->th_dport);
+			break;
+		default:
+			init(IP6_NXT_HDR(packet),
+				 IP6Address(IP6(packet)->ip6_src), 
+				 IP6Address(IP6(packet)->ip6_dst), 0, 0);
+			break;
+		} break;
 	default:
-		init(IP(packet)->ip_p,
-			 IP(packet)->ip_src.s_addr, IP(packet)->ip_dst.s_addr,
-			 0, 0);
-		break;
+		break;	
+
 	}
+
 }
 
 
-ConnectionID3::ConnectionID3(const u_char* packet,
-							 int wildcard_port) {
-	switch (IP(packet)->ip_p) {
-	case IPPROTO_UDP:
-		if (wildcard_port) 
-			init(IP(packet)->ip_p,
-				 IP(packet)->ip_src.s_addr, IP(packet)->ip_dst.s_addr,
-				 UDP(packet)->uh_dport);
-		else
-			init(IP(packet)->ip_p,
-				 IP(packet)->ip_dst.s_addr, IP(packet)->ip_src.s_addr,
-				 UDP(packet)->uh_sport);
-		break;
-	case IPPROTO_TCP:
-		if (wildcard_port) 
-			init(IP(packet)->ip_p,
-				 IP(packet)->ip_src.s_addr, IP(packet)->ip_dst.s_addr,
-				 TCP(packet)->th_dport);
-		else
-			init(IP(packet)->ip_p,
-				 IP(packet)->ip_dst.s_addr, IP(packet)->ip_src.s_addr,
-				 TCP(packet)->th_sport);
-		break;
-	default:
-		if (wildcard_port) 
-			init(IP(packet)->ip_p,
-				 IP(packet)->ip_src.s_addr, IP(packet)->ip_dst.s_addr,
-				 0);
-		else
-			init(IP(packet)->ip_p,
-				 IP(packet)->ip_dst.s_addr, IP(packet)->ip_src.s_addr,
-				 0);
-		break;
+ConnectionID3::ConnectionID3(const u_char* packet, int wildcard_port) {
+	
+	switch (IPV(packet)) {
+	case IPv4:
+		switch (IP(packet)->ip_p) {
+		case IPPROTO_UDP:
+			if (wildcard_port) 
+				init(IP(packet)->ip_p,
+					 IP6Address(IP(packet)->ip_src.s_addr), 
+				 	 IP6Address(IP(packet)->ip_dst.s_addr),
+					 UDP(packet)->uh_dport);
+			else
+				init(IP(packet)->ip_p,
+					 IP6Address(IP(packet)->ip_dst.s_addr), 
+					 IP6Address(IP(packet)->ip_src.s_addr),
+					 UDP(packet)->uh_sport);
+			break;
+		case IPPROTO_TCP:
+			if (wildcard_port) 
+				init(IP(packet)->ip_p,
+					 IP6Address(IP(packet)->ip_src.s_addr), 
+					 IP6Address(IP(packet)->ip_dst.s_addr),
+					 TCP(packet)->th_dport);
+			else
+				init(IP(packet)->ip_p,
+					 IP6Address(IP(packet)->ip_dst.s_addr), 
+					 IP6Address(IP(packet)->ip_src.s_addr),
+					 TCP(packet)->th_sport);
+			break;
+		default:
+			if (wildcard_port) 
+				init(IP(packet)->ip_p,
+					 IP6Address(IP(packet)->ip_src.s_addr), 
+					 IP6Address(IP(packet)->ip_dst.s_addr),
+					 0);
+			else
+				init(IP(packet)->ip_p,
+					 IP6Address(IP(packet)->ip_dst.s_addr), 
+					 IP6Address(IP(packet)->ip_src.s_addr),
+					 0);
+			break;
+		}break;
+	case IPv6:
+		switch (IP6_NXT_HDR(packet)) {
+		case IPPROTO_UDP:
+			if (wildcard_port) 
+				init(IP6_NXT_HDR(packet),
+					 IP6Address(IP6(packet)->ip6_src), 
+					 IP6Address(IP6(packet)->ip6_dst),
+					 UDP(packet)->uh_dport);
+			else
+				init(IP6_NXT_HDR(packet),
+					 IP6Address(IP6(packet)->ip6_dst), 
+					 IP6Address(IP6(packet)->ip6_src),
+					 UDP(packet)->uh_sport);
+			break;
+		case IPPROTO_TCP:
+			if (wildcard_port) 
+				init(IP6_NXT_HDR(packet),
+					 IP6Address(IP6(packet)->ip6_src), 
+					 IP6Address(IP6(packet)->ip6_dst),
+					 TCP(packet)->th_dport);
+			else
+				init(IP6_NXT_HDR(packet),
+					 IP6Address(IP6(packet)->ip6_dst), 
+					 IP6Address(IP6(packet)->ip6_src),
+					 TCP(packet)->th_sport);
+			break;
+		default:
+			if (wildcard_port) 
+				init(IP6_NXT_HDR(packet),
+					 IP6Address(IP6(packet)->ip6_src), 
+					 IP6Address(IP6(packet)->ip6_dst),
+					 0);
+			else
+				init(IP6_NXT_HDR(packet),
+					 IP6Address(IP6(packet)->ip6_dst), 
+					 IP6Address(IP6(packet)->ip6_src),
+					 0);
+			break;
+		}break;
+	default: break;
 	}
 }
 
 
 ConnectionID2::ConnectionID2(const u_char* packet) {
-	init(IP(packet)->ip_src.s_addr, IP(packet)->ip_dst.s_addr);
+	switch (IPV(packet)) {
+	case IPv4: 
+		init(IP6Address(IP(packet)->ip_src.s_addr), 
+			IP6Address(IP(packet)->ip_dst.s_addr));
+		break;
+	case IPv6: 
+		init(IP6Address(IP6(packet)->ip6_src), 
+			IP6Address(IP6(packet)->ip6_dst));
+		break;
+	default: return;
+	}
 }
 
 
@@ -178,25 +532,19 @@ std::string ConnectionID4::getStr() const {
 
 	std::stringstream ss;
 
-	uint32_t s_ip=v.ip1; //get_s_ip();
-	uint32_t d_ip=v.ip2; //get_d_ip();
+	//uint32_t s_ip=v.ip1; 
+	//uint32_t d_ip=v.ip2; 
 
 	ss << " ConnectionID4 "
 	/*
 	 << " Proto " << 0+get_proto()
 	 << " canonified " << get_is_canonified() << " "
 	*/
-	<< (UCP(s_ip)[0] & 0xff) << "."
-	<< (UCP(s_ip)[1] & 0xff) << "."
-	<< (UCP(s_ip)[2] & 0xff) << "."
-	<< (UCP(s_ip)[3] & 0xff)
+	<< v.ip1.AsURIString()
 	<< ":"
 	<< ntohs(get_port1())
 	<< " - "
-	<< (UCP(d_ip)[0] & 0xff) << "."
-	<< (UCP(d_ip)[1] & 0xff) << "."
-	<< (UCP(d_ip)[2] & 0xff) << "."
-	<< (UCP(d_ip)[3] & 0xff)
+	<< v.ip2.AsURIString()
 	<< ":"
 	<< ntohs(get_port2());
 	return ss.str();
@@ -208,19 +556,13 @@ std::string ConnectionID3::getStr() const {
 
 	std::stringstream ss;
 
-	uint32_t s_ip=get_ip1();//get_s_ip();
-	uint32_t d_ip=get_ip2();//get_d_ip();
+	//uint32_t s_ip=get_ip1();//get_s_ip();
+	//uint32_t d_ip=get_ip2();//get_d_ip();
 
 	ss << " ConnectionID3 "
-	<< (UCP(s_ip)[0] & 0xff) << "."
-	<< (UCP(s_ip)[1] & 0xff) << "."
-	<< (UCP(s_ip)[2] & 0xff) << "."
-	<< (UCP(s_ip)[3] & 0xff)
+	<< v.ip1.AsURIString()
 	<< " - "
-	<< (UCP(d_ip)[0] & 0xff) << "."
-	<< (UCP(d_ip)[1] & 0xff) << "."
-	<< (UCP(d_ip)[2] & 0xff) << "."
-	<< (UCP(d_ip)[3] & 0xff)
+	<< v.ip2.AsURIString()
 	<< ":"
 	<< get_port();
 	return ss.str();
@@ -231,19 +573,13 @@ std::string ConnectionID2::getStr() const {
 
 	std::stringstream ss;
 
-	uint32_t s_ip=get_ip1();//get_s_ip();
-	uint32_t d_ip=get_ip2();//get_d_ip();
+	//uint32_t s_ip=get_ip1();//get_s_ip();
+	//uint32_t d_ip=get_ip2();//get_d_ip();
 
 	ss << " ConnectionID2 "
-	<< (UCP(s_ip)[0] & 0xff) << "."
-	<< (UCP(s_ip)[1] & 0xff) << "."
-	<< (UCP(s_ip)[2] & 0xff) << "."
-	<< (UCP(s_ip)[3] & 0xff)
+	<< v.ip1.AsURIString()
 	<< " - "
-	<< (UCP(d_ip)[0] & 0xff) << "."
-	<< (UCP(d_ip)[1] & 0xff) << "."
-	<< (UCP(d_ip)[2] & 0xff) << "."
-	<< (UCP(d_ip)[3] & 0xff);
+	<< v.ip2.AsURIString();
 	return ss.str();
 }
 
@@ -271,6 +607,7 @@ ConnectionID4* ConnectionID4::parse(const char *str) {
 			htons(src_port), htons(dst_port));
 }
 
+/* technically dont need packet */
 void Connection::addPkt(const struct pcap_pkthdr* header, const u_char* packet) {
 	last_ts=to_tm_time(&header->ts);
 	tot_pkts++;
